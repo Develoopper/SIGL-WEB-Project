3131f51d4173cd4b68a83e70a8d8278c9290036d
oussama khalfi <okhalfi2000@gmail.com>
2021-02-26 13:54:25 +0100

    hana pullit o pushit

--- Views/Accueil.view/accueil.php
+++ Views/Accueil.view/accueil.php
@@ -1,34 +1,159 @@
+<?php
+  $slider = [
+    [
+      "title" => "First slide label",
+      "content" => "Some representative placeholder content for the first slide.",
+      "img" => "https://jennygracejewellery.co.uk/wp-content/uploads/2019/10/silver-fan-jewellery-header-e1604778854767-1200x360.jpg"
+    ],
+    [
+      "libellee" => "Second slide label",
+      "prix" => "Some representative placeholder content for the first slide.",
+      "img" => "https://cdn.shopify.com/s/files/1/0811/2085/collections/under100banner_1200x1200.jpg?v=1604329232"
+    ],
+    [
+      "libellee" => "Third slide label",
+      "prix" => "Some representative placeholder content for the first slide.",
+      "img" => "https://img1.wsimg.com/isteam/ip/b93c21eb-4bec-4f15-8616-1a0af4725974/ols/KlaarSmithDesign%20heart%20pendant%20bar.jpg/:/rs=w:1200,h:1200"
+    ],
+  ];
+
+  $meilleursVentes = [
+    [
+      "libellee" => "Montre water-proof taille standard",
+      "prix" => 579.00,
+      "img" => "https://ma.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/24/177193/1.jpg?3360"
+    ],
+    [
+      "libellee" => "Montre water-proof taille standard",
+      "prix" => 579.00,
+      "img" => "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/42/139382/1.jpg?9659"
+    ],
+    [
+      "libellee" => "Montre water-proof taille standard",
+      "prix" => 579.00,
+      "img" => "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/16/295183/1.jpg?1806"
+    ],
+    [
+      "libellee" => "Montre water-proof taille standard",
+      "prix" => 579.00,
+      "img" => "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/71/544163/1.jpg?0556"
+    ],
+    [
+      "libellee" => "Montre water-proof taille standard",
+      "prix" => 579.00,
+      "img" => "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/68/344182/1.jpg?2292"
+    ],
+    [
+      "libellee" => "Montre water-proof taille standard",
+      "prix" => 579.00,
+      "img" => "https://ma.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/85/144613/1.jpg?2808"
+    ],
+  ];
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <title>Accueil</title>
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing Script">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+  <link rel="stylesheet" href="accueil.css">
+  <title>Document</title>
   <style>
-    <?php include "./accueil.css"; ?>
-    <?php include "../assets/materialize/css/materialize.min.css"; ?>
+    body {
+      font-family: Montserrat;
+      font-size: 15px;
+    }
+
+    .shadow-effect {
+      /* box-shadow: 0px 0px 0px grey; */
+      -webkit-transition: box-shadow .2s ease-out;
+      box-shadow: .0px .0px 1px grey;
+    }
+
+    .shadow-effect:hover { 
+      box-shadow: 0px 0px 5px grey;
+      -webkit-transition: box-shadow .12s ease-in;
+      cursor: pointer;
+    }
   </style>
 </head>
 <body>
   <!-- Nav bar -->
-  <nav>
-    <div class="nav-wrapper nav-bar">
-      <a href="#" class="brand-logo center">
-        <img class="logo" src="assets/img/logo.png" style="margin-top: 5px;">
-      </a>
-
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="#">Femmes</a></li>
-        <li><a href="#">Hommes</a></li>
-        <li><a href="#">Garçons</a></li>
-        <li><a href="#">Filles</a></li>
-      </ul>
-      <a href="#" class="brand-logo right">
-        <span class='left' style='font-size: 20px; margin-right: 7'>
-          <?php echo $name; ?>
-        </span>
-        <i class="material-icons" style="font-size: 30px">account_circle</i>
-      </a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid mx-3">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" style="font-family: 'Dancing Script'; font-size: 35px;" href="#">Elegance</a>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Femmes
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Hommes
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Filles
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Garçons
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        
+        <div class="d-flex align-items-center justify-content-between" style="font-size: 15px;">
+          <form class="d-flex me-2">
+            <input class="form-control me-2" style="font-size: 14px;"  type="search" placeholder="Chercher un produit" aria-label="Search">
+            <button class="btn btn-outline-dark" style="font-size: 14px;" type="submit">Rechercher</button>
+          </form>
+            <a href="#" class="d-flex align-items-center mx-3 text-dark" style="text-decoration: none;">
+              <i class= material-icons mx-1" style="font-size: 30px;">account_circle</i>
+              Compte
+            </a>
+            <a href="#" class="d-flex align-items-center text-dark" style="text-decoration: none;">
+              <i class="large material-icons mx-1" style="font-size: 30px;">shopping_cart</i>
+              Panier
+            </a>
+        </div>
+      </div>
     </div>
   </nav>
 
