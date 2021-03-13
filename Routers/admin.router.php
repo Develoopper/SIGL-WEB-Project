d21b9f5eca44714fd71156277fd6a08b0c34534b
oussama khalfi <okhalfi2000@gmail.com>
2021-03-13 16:21:20 +0100

    new user

--- Routers/admin.router.php
+++ Routers/admin.router.php
@@ -1,21 +1,21 @@
 <?php
-  Route::set("adminUtilisateurs", function() {
+  Route::set("adminUtilisateurs", "Auth", function() {
     Controller::CreateView("AdminUtilisateurs", []);
   });
 
-  Route::set("adminProduits", function() {
+  Route::set("adminProduits", "Auth", function() {
     Controller::CreateView("AdminProduits", []);
   });
 
-  Route::set("adminCommandes", function() {
+  Route::set("adminCommandes", "Auth", function() {
     Controller::CreateView("AdminCommandes", []);
   });
 
-  Route::set("adminCategories", function() {
+  Route::set("adminCategories", "Auth", function() {
     Controller::CreateView("AdminCategories", []);
   });
 
-  Route::set("adminSousCategories", function() {
+  Route::set("adminSousCategories", "Auth", function() {
     Controller::CreateView("AdminSousCategories", []);
   });
 
